0c415573fe5d5818032f5d921056d4f926347696
Carlos Aldazosa <carlos.aldazosa@outlook.com>
2022-07-20 16:24:57 -0400

    Portfolio page completed

--- main.js
+++ main.js
@@ -11,3 +11,24 @@ menuIcon.addEventListener('click', () => {
 menuClose.addEventListener('click', () => { 
   display(menu, menuClass);
 });
+
+const project = document.querySelectorAll('.project__banner');
+const projectButtons = document.querySelectorAll('.project__buttons');
+console.log(projectButtons);
+
+for (let element = 0; element < project.length; element++) {
+  project[element].addEventListener('mousemove', () => {
+    projectButtons[element].classList.add('project__buttons--active');
+    project[element].classList.add('project__banner--active')
+  });
+
+  projectButtons[element].addEventListener('mouseenter', () => {
+    projectButtons[element].classList.add('project__buttons--active');
+    project[element].classList.add('project__banner--active')
+  })
+
+  project[element].addEventListener('mouseleave', () => {
+    projectButtons[element].classList.remove('project__buttons--active');
+    project[element].classList.remove('project__banner--active')
+})
+}
