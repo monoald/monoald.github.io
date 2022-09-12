884d275fb974b6b996392c41c48575a062a0edb4
Carlos S Aldazosa <carlos.aldazosa@outlook.com>
2022-09-12 17:34:22 -0400

    [UPDATE] Refactorizing

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import display from './utils/display.js';
+import showSelectProjects from './utils/projectsSelect.js';
 
+// Toggle Menu
 const menu = document.getElementById('menu');
 const menuClass = menu.className;
 const menuIcon = document.getElementById('menu-icon');
@@ -12,22 +14,7 @@ menuClose.addEventListener('click', () => {
   display(menu, menuClass);
 });
 
-const project = document.querySelectorAll('.project__banner');
-const projectButtons = document.querySelectorAll('.project__buttons');
+// Select project
+const buttonContainer = document.querySelector('#skills-container');
 
-for (let element = 0; element < project.length; element++) {
-  project[element].addEventListener('mousemove', () => {
-    projectButtons[element].classList.add('project__buttons--active');
-    project[element].classList.add('project__banner--active')
-  });
-
-  projectButtons[element].addEventListener('mouseenter', () => {
-    projectButtons[element].classList.add('project__buttons--active');
-    project[element].classList.add('project__banner--active')
-  })
-
-  project[element].addEventListener('mouseleave', () => {
-    projectButtons[element].classList.remove('project__buttons--active');
-    project[element].classList.remove('project__banner--active')
-})
-}
+buttonContainer.addEventListener('click', showSelectProjects);
--- utils/projectsSelect.js
+++ utils/projectsSelect.js
@@ -1,9 +1,7 @@
 import { data }  from '../data/projects/projects.js';
+import addProject from './addProject.js';
 
-const buttonContainer = document.querySelector('#skills-container');
 const projectContainer = document.querySelector('#projects-container');
-
-buttonContainer.addEventListener('click', showSelectProjects);
 
 function showSelectProjects(event) {
   const technology = event.target.textContent;
@@ -29,63 +27,4 @@ function showSelectProjects(event) {
   projectContainer.appendChild(fragment);
 }
 
-function addProject(project) {
-  const container = document.createElement('div');
-  container.classList.add('project');
-  container.id = project.id;
-
-  const picture = document.createElement('picture');
-  picture.classList.add('project__banner');
-
-  const image = document.createElement('img');
-  image.classList.add('project__image')
-  image.src = project.image;
-  image.alt = `${project.name} built by Carlos S. Aldazosa`;
-  image.width = 280;
-  image.height = 176;
-  
-  picture.appendChild(image);
-
-  const data = document.createElement('div');
-  data.classList.add('project__data');
-
-  const title = document.createElement('h3');
-  title.classList.add('project__title');
-  title.textContent = project.name;
-
-  const tech = document.createElement('p');
-  tech.classList.add('project__tech')
-  tech.innerHTML = `<b>Technologies:</b> ${project.technologies.join(', ') }`;
-
-  const description = document.createElement('article');
-  description.classList.add('project__description');
-  description.textContent = project.description;
-
-  const projectButtons = document.createElement('div');
-  projectButtons.classList.add('project__buttons');
-
-  const deployButton = document.createElement('a');
-  deployButton.classList.add('primary-button');
-  deployButton.target = '_blank';
-  deployButton.href = project['link-deploy'];
-  deployButton.textContent = 'Open Project';
-
-  const repositoryButton = document.createElement('a');
-  repositoryButton.classList.add('primary-button');
-  repositoryButton.target = '_blank';
-  repositoryButton.href = project['link-repository'];
-  repositoryButton.textContent = 'Repository';
-
-  projectButtons.appendChild(deployButton);
-  projectButtons.appendChild(repositoryButton);
-
-  data.appendChild(title);
-  data.appendChild(tech);
-  data.appendChild(description);
-  data.appendChild(projectButtons);
-
-  container.appendChild(picture);
-  container.appendChild(data);
-
-  return container;
-}
+export default showSelectProjects;
