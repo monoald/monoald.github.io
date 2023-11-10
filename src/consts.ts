6d8f9ae4a2e0b03594832a1e463dffc48c8f9e36
Carlos S. Aldazosa <carlos.aldazosa@outlook.com>
2023-11-09 23:48:47 -0400

    [UPDATE] base home page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,93 @@
+import html from "./icons/html5.svg"
+import css from "./icons/css.svg"
+import javaScript from "./icons/javascript.svg"
+import typeScript from "./icons/typescript.svg"
+import tailwind from "./icons/tailwindcss.svg"
+import react from "./icons/react.svg"
+import redux from "./icons/redux.svg"
+import next from "./icons/nextjs.svg"
+import astro from "./icons/astro.svg"
+import node from "./icons/nodejs.svg"
+import express from "./icons/expressjs.svg"
+import graphQl from "./icons/graphql.svg"
+import deno from "./icons/deno.svg"
+import hono from "./icons/hono.svg"
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Monoald'
+export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const frontSkills = [
+  {
+    name: 'HTML',
+    icon: html,
+    color: '#EF652A'
+  },
+  {
+    name: 'CSS',
+    icon: css,
+    color: '#30A9DC'
+  },
+  {
+    name: 'JavaScript',
+    icon: javaScript,
+    color: '#F0DB4F'
+  },
+  {
+    name: 'TypeScript',
+    icon: typeScript,
+    color: '#3178C6'
+  },
+  {
+    name: 'Tailwind',
+    icon: tailwind,
+    color: '#17BAB8'
+  },
+  {
+    name: 'React',
+    icon: react,
+    color: '#00D8FF'
+  },
+  {
+    name: 'Redux',
+    icon: redux,
+    color: '#764ABC',
+  },
+  {
+    name: 'Next',
+    icon: next,
+    color: '#FAFAFA'
+  },
+  {
+    name: 'Astro',
+    icon: astro,
+    color: '#FAFAFA'
+  }
+]
+export const backSkills = [
+  {
+    name: 'NodeJS',
+    icon: node,
+    color: '#539E43'
+  },
+  {
+    name: 'ExpressJS',
+    icon: express,
+    color: '#FAFAFA'
+  },
+  {
+    name: 'GraphQL',
+    icon: graphQl,
+    color: '#F6009C'
+  },
+  {
+    name: 'Deno',
+    icon: deno,
+    color: '#FAFAFA'
+  },
+  {
+    name: 'Hono',
+    icon: hono,
+    color: '#FD5A11'
+  }
+]
