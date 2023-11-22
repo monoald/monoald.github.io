1a7f8f1e2d28047a121e4c3d626397310248a6b2
Carlos S. Aldazosa <carlos.aldazosa@outlook.com>
2023-11-21 23:17:44 -0400

    [UPDATE] project section

--- src/consts.ts
+++ src/consts.ts
@@ -1,131 +1,103 @@
-import html from './icons/html5.svg'
-import css from './icons/css.svg'
-import javaScript from './icons/javascript.svg'
-import typeScript from './icons/typescript.svg'
-import tailwind from './icons/tailwindcss.svg'
-import react from './icons/react.svg'
-import redux from './icons/redux.svg'
-import next from './icons/nextjs.svg'
-import astro from './icons/astro.svg'
-import node from './icons/nodejs.svg'
-import express from './icons/expressjs.svg'
-import graphQl from './icons/graphql.svg'
-import deno from './icons/deno.svg'
-import hono from './icons/hono.svg'
-import jasmine from './icons/jasmine.svg'
-import jest from './icons/jest.svg'
-import testingLibrary from './icons/testinglibrary.svg'
-import git from './icons/git.svg'
-import linux from './icons/linux.png'
-import vsCode from './icons/vscode.svg'
-import figma from './icons/figma.svg'
-
 export const SITE_TITLE = 'Monoald'
 export const SITE_DESCRIPTION = 'Welcome to my website!'
+import colorsKitLogo from './icons/colors-kit-logo.svg'
+
 export const stack = [
   {
     name: 'HTML',
-    icon: html,
     color: '#EF652A'
   },
   {
     name: 'CSS',
-    icon: css,
     color: '#30A9DC'
   },
   {
     name: 'JavaScript',
-    icon: javaScript,
     color: '#F0DB4F'
   },
   {
     name: 'TypeScript',
-    icon: typeScript,
     color: '#3178C6'
   },
   {
     name: 'Tailwind',
-    icon: tailwind,
     color: '#17BAB8'
   },
   {
     name: 'React',
-    icon: react,
     color: '#00D8FF'
   },
   {
     name: 'Redux',
-    icon: redux,
     color: '#764ABC',
   },
   {
     name: 'Next',
-    icon: next,
     color: '#FAFAFA'
   },
   {
     name: 'Astro',
-    icon: astro,
     color: '#FAFAFA'
   },
   {
     name: 'NodeJS',
-    icon: node,
     color: '#539E43'
   },
   {
     name: 'ExpressJS',
-    icon: express,
     color: '#FAFAFA'
   },
   {
     name: 'GraphQL',
-    icon: graphQl,
     color: '#F6009C'
   },
   {
     name: 'Deno',
-    icon: deno,
     color: '#FAFAFA'
   },
   {
     name: 'Hono',
-    icon: hono,
     color: '#FD5A11'
   },
   {
     name: 'Jasmine',
-    icon: jasmine,
     color: '#8A4182'
   },
   {
     name: 'Jest',
-    icon: jest,
     color: '#99425B'
   },
   {
     name: 'Testing Library',
-    icon: testingLibrary,
     color: '#DC312F'
   },
   {
     name: 'Git',
-    icon: git,
     color: '#DE4C36'
   },
   {
     name: 'Linux',
-    icon: linux,
     color: '#DFAF22'
   },
   {
     name: 'VS Code',
-    icon: vsCode,
     color: '#3DA9F2'
   },
   {
     name: 'Figma',
-    icon: figma,
     color: '#A95FFF'
   }
+]
+
+export const projects = [
+  {
+    name: 'Colors Kit',
+    description: 'Colors Kit is an <b>npm package</b> for color manipulation in formats like CMYK, HSL, RGB, etc. With it, you can convert a color between formats, simulate color blindness, generate all sorts of palettes, extract colors from an image, and more.',
+    image: colorsKitLogo
+  },
+  {
+    name: 'Colors Kit',
+    description: 'Colors Kit is an <b>npm package</b> for color manipulation in formats like CMYK, HSL, RGB, etc. With it, you can convert a color between formats, simulate color blindness, generate all sorts of palettes, extract colors from an image, and more.',
+    image: colorsKitLogo
+  },
 ]
