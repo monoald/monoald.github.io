6de008c73bedb535f7e97183650f8465881fba94
Carlos S Aldazosa <carlos.aldazosa@outlook.com>
2024-04-24 23:51:12 -0400

    [FEAT] Linux Stuff section

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,27 @@
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  // Type-check frontmatter using a schema
+  type: "content",
   schema: z.object({
     id: z.string(),
     title: z.string(),
     description: z.string(),
     categories: z.array(z.string()),
-    // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string(),
   }),
 });
 
-export const collections = { blog };
+const linux = defineCollection({
+  type: "content",
+  schema: z.object({
+    id: z.string(),
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+});
+
+export const collections = { blog: blog, linux: linux };
