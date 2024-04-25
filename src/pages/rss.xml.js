426ae23837aac719b541afa086099427804ed122
Carlos S Aldazosa <carlos.aldazosa@outlook.com>
2024-04-24 23:51:12 -0400

    [FEAT] Linux Stuff section

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,24 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		categories: [],
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection("blog");
+  const scripts = await getCollection("linux");
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    categories: [],
+    items: [
+      ...posts.map((post) => ({
+        ...post.data,
+        link: `/blog/${post.slug}/`,
+      })),
+      ...scripts.map((script) => ({
+        ...script.data,
+        link: `/linux/${script.slug}/`,
+      })),
+    ],
+  });
 }
